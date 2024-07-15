5c49b1a807a26c64b940fb74e1996da64bacd5ca
UmerSaleemUltra <us8000750@gmail.com>
2024-07-16 03:32:25 +0500

    Website Router Bug Solved

--- src/Confing/Router.jsx
+++ src/Confing/Router.jsx
@@ -5,27 +5,27 @@ import Detail from '../Views/Detail';
 import Signup from '../Views/Signup';
 import Login from '../Views/Login';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-  {
-    path: "/detail/:id",
-    element: <Detail />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
-
 const Router = () => {
-  return <RouterProvider router={router} />;
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Dashboard />,
+        },
+        {
+            path: "/detail/:id",
+            element: <Detail />,
+        },
+        {
+            path: "/signup",
+            element: <Signup />,
+        },
+        {
+            path: "/Login",
+            element: <Login />,
+        },
+    ]);
+
+    return <RouterProvider router={router} />;
 };
 
 export default Router;
