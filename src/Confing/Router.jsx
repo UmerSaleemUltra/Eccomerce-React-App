ee70580d8b8a80adae57fd917ff78fc3803f86b9
UmerSaleemUltra <us8000750@gmail.com>
2024-07-16 02:30:34 +0500

    Add Vercel.json File To Work react-router-dom properly

--- src/Confing/Router.jsx
+++ src/Confing/Router.jsx
@@ -1,33 +1,31 @@
-// src/Config/Router.jsx
-
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Dashboard from '../Views/Dashboard';
-import Detail from '../Views/Details';
+import Detail from '../Views/Detail';
 import Signup from '../Views/Signup';
 import Login from '../Views/Login';
 
-const Router = () => {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <Dashboard />,
-        },
-        {
-            path: "/detail/:id",
-            element: <Detail />,
-        },
-        {
-            path: "/signup",
-            element: <Signup />,
-        },
-        {
-            path: "/Login",
-            element: <Login />,
-        },
-    ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/detail/:id",
+    element: <Detail />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
-    return <RouterProvider router={router} />;
+const Router = () => {
+  return <RouterProvider router={router} />;
 };
 
 export default Router;
